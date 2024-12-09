2d077bbbc85fae9eea4186980bbf57efaf588817
keyurpatil06 <keyurpatil3000@gmail.com>
2024-12-09 15:31:23 +0530

    update UI

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <span className="tracking-wider">Made by </span>
       <Link
         href="https://github.com/keyurpatil06"
-        className="underline text-purple-400 font-semibold tracking-wide"
+        className="underline text-purple-400 font-semibold tracking-wide whitespace-nowrap"
         target="_blank"
       >
         Keyur Patil
