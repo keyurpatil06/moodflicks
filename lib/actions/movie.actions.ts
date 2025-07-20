7531e63298079f6bfb49ee9adeb11bc9b84d2fed
keyurpatil06 <keyurpatil3000@gmail.com>
2025-07-20 09:12:32 +0530

    implement caching

--- lib/actions/movie.actions.ts
+++ lib/actions/movie.actions.ts
@@ -2,10 +2,16 @@
 
 import { moodToGenreMap } from "@/data";
 import axios from "axios";
+import { getFromCache, setInCache } from "../utils";
 
 const { TMDB_KEY, WEB_URL } = process.env;
 
 export const getMoviesByGenre = async (category: string) => {
+    const cachedMovies = getFromCache(category);
+    if(cachedMovies){
+        return cachedMovies;
+    }
+
     const genres = moodToGenreMap[category].join(',');
     // const url = `${WEB_URL}?api_key=${TMDB_KEY}&with_genres=${genres}&sort_by=popularity.desc`;
     const url = `${WEB_URL}?api_key=${TMDB_KEY}&with_genres=${genres}`;
@@ -18,8 +24,11 @@ export const getMoviesByGenre = async (category: string) => {
         // Resolve all requests
         const responses = await Promise.all(requests);
 
-        // // Combine all results into a single array
+        // Combine all results into a single array
         const movies = responses.flatMap(response => response.data.results);
+
+        // cache the movies
+        setInCache(category, movies);
         
         return movies;
     } catch (error) {
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+const movieCache = new Map<string, any>();
+
+export const getFromCache = (key: string) => movieCache.get(key);
+export const setInCache = (key: string, value: any) => movieCache.set(key, value);
